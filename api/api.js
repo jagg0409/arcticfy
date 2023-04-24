497709bd4db70a8c304460da52d810c3f673f97b
g2 <jg2nano@gmail.com>
2023-04-24 10:56:14 -0500

    audio player casi listo

--- api/api.js
+++ api/api.js
@@ -1,31 +1,54 @@
 const express = require('express')
-const mongoose =require('mongoose')
+const mongoose = require('mongoose')
 const user = require('./user.controler')
 const app = express()
+const { auth, isAuthenticated } = require('./auth.controler')
+//const auth2 = require('./auth.controler-prueba')
 const port = 2000
 
 app.use(express.json())
-  
+
 mongoose.connect('mongodb+srv://jagg0409:lolazo0409@arcticfy.6tbtlze.mongodb.net/?retryWrites=true&w=majority')
 
-app.get('/:username', user.get)
-   
+app.get('/try', (req, res) => {
+    if (isAuthenticated) {
+        res.sendFile(`${__dirname}/web/html/index.html`)
+
+    } else {
+        res.sendFile(`${__dirname}/web/html/sesion.html`)
+    }
+})
+app.get('/prueba', isAuthenticated, (req, res) => {
+    res.sendFile(`${__dirname}/web/html/index.html`)
+
+});
+
+app.get('/:email', isAuthenticated, user.get)
+
 app.options('/', user.list)
 
-app.post('/', user.create)
+app.post('/email', isAuthenticated, user.create)
 
-app.put('/:username', user.update)
+app.post('/login', auth.login)
 
-app.patch('/:id', user.update)
+app.post('/register', auth.register)
 
-app.delete('/:username', user.delete)
+app.put('/:email', isAuthenticated, user.update)
 
-app.use (express.static('web'))
+app.patch('/:id', isAuthenticated, user.update)
+
+app.delete('/:email', isAuthenticated, user.delete)
+
+app.use(express.static('web'))
 
 app.get('/', (req, res) =>{
-    res.sendFile(`${__dirname}/web/html/registro.html`)
+    res.sendFile(`${__dirname}/web/html/index.html`)
 })
 
-app.listen(port, () =>{
-    console.log ('arrancando')
+
+
+app.listen(port, () => {
+    console.log('arrancando')
 })
+
+
